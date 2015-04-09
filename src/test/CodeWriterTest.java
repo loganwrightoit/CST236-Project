adc4c4d6960b822d9aebbc37cec2658f0dc1f9ec
Mineshopper <kmartshopper@gmail.com>
2015-04-09 04:00:01 -0700

    Add project.

---
+++ src/test/CodeWriterTest.java
@@ -0,0 +1,48 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import main.CodeWriter;
+import main.CodeWriter.CodeWriterRunnable;
+
+import org.junit.Test;
+
+public class CodeWriterTest {
+
+	@SuppressWarnings("static-access")
+	@Test
+	/**
+	 * Ensure main is executable.
+	 */
+	public void testMain()
+	{
+		CodeWriter main = CodeWriterRunnable.getInstance();
+		main.main(new String[] { "1", "2" });
+	}
+
+	@Test
+	/**
+	 * Test that main creates a runnable instance of program.
+	 */
+	public void testRunnableInstance()
+	{
+		CodeWriterRunnable runnable = CodeWriter.getRunnable();
+		assertNotNull(runnable);
+		runnable.run(); // Will create instance and set window to visible
+		assertEquals(CodeWriterRunnable.getInstance().isVisible(), true);
+	}
+
+	@Test
+	/**
+	 * Test GUI initialization parameters.
+	 */
+	public void testGuiInit()
+	{
+		CodeWriter main = CodeWriterRunnable.getInstance();
+		main.initUserInterface();
+		assertEquals(main.getTitle(), "CodeWriter");
+	}
+
+
+
+}
---
+++ src/test/util/TokenizerTest.java
@@ -0,0 +1,18 @@
+package test.util;
+
+import main.util.Tokenizer;
+
+import org.junit.Test;
+
+public class TokenizerTest {
+
+	@Test
+	/**
+	 * Unimplemented coverage test.
+	 */
+	public void test()
+	{
+		Tokenizer tk = new Tokenizer();
+	}
+
+}
