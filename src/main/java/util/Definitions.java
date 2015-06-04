192d59d1c66c693ef4e6ced2c755bdc4d8873e79
Mineshopper <kmartshopper@gmail.com>
2015-06-03 21:37:31 -0700

    Add a little documentation.

--- src/main/java/util/Definitions.java
+++ src/main/java/util/Definitions.java
@@ -10,45 +10,52 @@ import main.java.gui.instructions.StyleInstruction;
 
 public class Definitions {
 
-	private static Hashtable<String, Class<? extends IInstruction>> keywords;
-	static {
-		keywords = new Hashtable<String, Class<? extends IInstruction>>();
-		keywords.put("color", ColorInstruction.class);
-		keywords.put("style", StyleInstruction.class);
-		keywords.put("font", FontInstruction.class);
-	}
+    // Stores keyword and associated instruction class type
+    private static Hashtable<String, Class<? extends IInstruction>> keywords;
+    static {
+        keywords = new Hashtable<String, Class<? extends IInstruction>>();
+        keywords.put("color", ColorInstruction.class);
+        keywords.put("style", StyleInstruction.class);
+        keywords.put("font", FontInstruction.class);
+    }
 
-	public IInstruction parseInstruction(String input)
-	{
-		StringTokenizer st = new StringTokenizer(input);
-		while (st.hasMoreTokens())
-		{
-			// Look for a keyword
-			String token = st.nextToken();
-			Class<? extends IInstruction> clazz = keywords.get(token);
+    /**
+     * Parses a string into an executable instruction.
+     *
+     * @param input
+     * @return an {@link IInstruction instruction}
+     */
+    public IInstruction parseInstruction(String input)
+    {
+        StringTokenizer st = new StringTokenizer(input);
+        while (st.hasMoreTokens())
+        {
+            // Look for a keyword
+            String token = st.nextToken();
+            Class<? extends IInstruction> clazz = keywords.get(token);
 
-			if (clazz != null)
-			{
-				try {
-					return getClassInstance(clazz);
-				} catch (Exception e) { }
-			}
-		}
+            if (clazz != null)
+            {
+                try {
+                    return getClassInstance(clazz);
+                } catch (Exception e) { }
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	/**
-	 * Grabs a new instance of the class and returns it.
-	 *
-	 * @param clazz
-	 * @return
-	 * @throws InstantiationException
-	 * @throws IllegalAccessException
-	 */
-	public IInstruction getClassInstance(Class<? extends IInstruction> clazz) throws InstantiationException, IllegalAccessException
-	{
-		return clazz.newInstance();
-	}
+    /**
+     * Grabs a new instance of the class and returns it.
+     *
+     * @param clazz
+     * @return an {@link IInstruction instruction}
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     */
+    public IInstruction getClassInstance(Class<? extends IInstruction> clazz) throws InstantiationException, IllegalAccessException
+    {
+        return clazz.newInstance();
+    }
 
 }
